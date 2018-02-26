70ed5ae7e038d98e6c14e4958e86a8f66c8c6dca
Vipul Barodiya <sonumeewa@gmail.com>
2018-02-26 19:58:13 +0530

    plans saved automatically once

--- api/controllers/plan.js
+++ api/controllers/plan.js
@@ -6,32 +6,7 @@ var jwt = require('jsonwebtoken');
 //http://localhost:8000/api/get/plans
 
 module.exports.getPlans = function(req,res){
-    plan0 = new Plan({
-        name : "trial",
-        cost : 0,
-        maxUsers : 3,
-        maxAdmins : 1,
-    });
-    plan1 = new Plan({
-        name : "Small",
-        cost : 5000,
-        maxUsers : 2,
-        maxAdmins : 1,
-    });
 
-    plan2 = new Plan({
-        name : "Medium",
-        cost : 10000,
-        maxUsers : 5,
-        maxAdmins : 2,
-    });
-
-    plan3 = new Plan({
-        name : "Large",
-        cost : 15000,
-        maxUsers : 11,
-        maxAdmins : 3,
-    });
 
     var plans = Plan.find({},null, {sort: {cost: 1}},function(err, plans){
 
@@ -39,10 +14,51 @@ module.exports.getPlans = function(req,res){
                 console.log("here" +err);
             }
             else{
-                res.send({
-                    success : true,
-                    plans : plans
-                }); 
+                if(plans.length== 0){
+                    plan0 = new Plan({
+                        name : "trial",
+                        cost : 0,
+                        maxUsers : 3,
+                        maxAdmins : 1,
+                    });
+                    plan1 = new Plan({
+                        name : "Small",
+                        cost : 5000,
+                        maxUsers : 2,
+                        maxAdmins : 1,
+                    });
+                
+                    plan2 = new Plan({
+                        name : "Medium",
+                        cost : 10000,
+                        maxUsers : 5,
+                        maxAdmins : 2,
+                    });
+                
+                    plan3 = new Plan({
+                        name : "Large",
+                        cost : 15000,
+                        maxUsers : 11,
+                        maxAdmins : 3,
+                    });
+                    plan0.save();
+                    plan1.save();
+                    plan2.save();
+                    plan3.save();
+
+                    res.send({
+                        success:true,
+                        plans:[plan0, plan1, plan2, plan3]
+                    })
+
+                }else{
+                    res.send({
+                        success : true,
+                        plans : plans
+                    }); 
+
+                }
+               
             }
     });
 };
