706d6e4d2c7f7932e03e27bfb2bbb55a250921a5
PranjalSrivastava092 <pranjalsri092@gmail.com>
2018-02-26 23:20:10 +0530

    Saved from Mathew

--- api/routes/router.js
+++ api/routes/router.js
@@ -9,8 +9,8 @@ router.post('/user/mobile', user.setMobile);
 router.post('/user/verify/mobile', user.verifyMobile);
 router.get('/user/verify/email/:id', user.verifyEmail);
 router.post('/user/verify/email', user.sendVerMail);
-router.get('/get/plans', plan.getPlans);
-router.post('/user/set/plan', user.setPlan);
+router.get('/plans', plan.getPlans);
+router.post('/user/plan', user.setPlan);
 router.post('/user/state', user.setState);
 router.get('/user/state', user.getState);
 
