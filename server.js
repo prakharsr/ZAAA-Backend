ae7b9414f98416a2b1dd6adec56fcb4a0edab19b
Vipul Barodiya <sonumeewa@gmail.com>
2018-03-10 20:54:37 +0530

    Patch added to header access control

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var path = require('path');
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT,PATCH, DELETE, OPTIONS');
     if (req.method === 'OPTIONS') {
         res.end();
     } else {
