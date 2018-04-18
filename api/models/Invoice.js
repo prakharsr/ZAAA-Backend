5289ef0fb9d325790f05b85b2fd4523104e54c0e
sonumeewa <sonumeewa@gmail.com>
2018-04-18 11:29:03 +0530

    schema initial for Invoice

--- api/models/Invoice.js
+++ api/models/Invoice.js
@@ -7,28 +7,68 @@ var twilioClient = require('twilio')(config.accountSid, config.authToken);
 
 
 var InvoiceSchema = new mongoose.Schema({
+    
+    InvoiceNo:String,
+    releaseOrderId:String,
     date: String,
-    invoiceNO: {
+    releaseOrderNO: {
         type:String,
         unique:true
     },
     agencyName: String,
+    agencyGSTIN: String,
+    agencyPin:String,
     agencyAddress:String,
     agencyState:String,
-    agencyGSTIN: String,
     agencyPerson: String,
     signature: String,
+
     publicationName:String,
     publicationEdition:String,
-    publicationAddress:String,
-    publicationCity:String,
+    mediaType:String,
     publicationState:String,
+    publicationGSTIN:String,
+    
     clientName:String,
-    clientAddress:String,
-    clientCity:String,
     clientState:String,
     clientGSTIN:String,
+
+    PremiumCustom:{
+        Amount:Number,
+        Percentage:Boolean,
+        PremiumType:String
+    },
+    PremiumBox:{
+        Amount:Number,
+        Included:Boolean
+    },
+    PremiumBaseColour:{
+        Amount:Number,
+        Included:Boolean
+    },
+    PremiumEmailId:{
+        Amount:Number,
+        Quantity:Number,
+        Included:Boolean
+    },
+    PremiumCheckMark:{
+        Amount:Number,
+        Included:Boolean
+    },
+    PremiumWebsite:{
+        Amount:Number,
+        Quantity:Number,
+        Included:Boolean
+    },
+    PremiumExtraWords:{
+        Amount:Number,
+        Quantity:Number,
+        Included:Boolean
+    },
+
     adType:String,
+    rate:String,
+    unit:String,
     adCategory1:String,
     adCategory2:String,
     adCategory3:String,
@@ -36,22 +76,60 @@ var InvoiceSchema = new mongoose.Schema({
     adCategory5:String,
     adCategory6:String,
     adHue:String,
-    adSize:String,
+    AdWords:String,
+    AdWordsMax:String,
+    AdDuration:String,
+    adSizeL:String,
+    adSizeW:String,
+    adSizeCustom:Boolean,
+    adSizeAmount:String,
+    AdTime:String,
     adTotalSpace:String,
     adEdition:String,
     adPosition:String,
-    adScheme:StEmailIdring,
+    adSchemePaid:String,
+    adSchemeFree:String,
     adTotal:String,
-    insertionDate:String,
+    
+    insertions:[{date:{day:String,month:String,year:String}, marked:Boolean}],
     adGrossAmount:String,
     publicationDiscount:String,
     agencyDiscount1:String,
     agencyDiscount2:String,
-    additonalCharges:String,
-    taxAmount:String,
+    taxAmount:{
+        primary:String,
+        secondary:String
+    },
+    taxIncluded:Boolean,
     netAmountFigures:String,
     netAmountWords:String,
+    caption:String,
     remark:String,
+
+    paymentType:String,
+    paymentDate:String,
+    paymentNo:String,
+    paymentAmount:String,
+    paymentBankName:String,
+    otherCharges:[
+    {
+        amount:String,
+        chargeType:String,
+    }
+    ],
+    executiveName:String,
+    executiveOrg:String,
+    otherRemark:String,
+    FinalAmount:String,
+    FinalTaxAmount:String,
+
+    additionalCharges:String,
+    additionalTax:String,
+    taxType:String,
+    
+
+
+
     firm : {
         type:mongoose.Schema.Types.ObjectId,
         ref:"Firm"
@@ -59,4 +137,3 @@ var InvoiceSchema = new mongoose.Schema({
     });
     module.exports = mongoose.model('Invoice', InvoiceSchema);
     
-
