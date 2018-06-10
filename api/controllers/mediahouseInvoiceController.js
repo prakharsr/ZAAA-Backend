bd82e7d0f04bbcef91a9c5e64c94f62a89904f25
sonumeewa <sonumeewa@gmail.com>
2018-06-11 02:32:41 +0530

    media house invoice reciept tested creation , for listing few changes can be done in order to list insetion from invoice

--- api/controllers/mediahouseInvoiceController.js
+++ api/controllers/mediahouseInvoiceController.js
@@ -19,40 +19,59 @@ var path = require('path');
 var perPage=20;
 
 module.exports.createMHInvoice = async (request,response) => {
-    var mediahouseID =await searchMediahouseID(request, response, user);
-    var clientID = await searchClientID(request, response, user);
-    var executiveID = await searchExecutiveID(request, response, user);
-    var releaseorder = await ReleaseOrder.findById(request.body.id);
-    var firm = await Firm.findById(user.firm)
-
-    var mhinvoice = new MediaHouseInvoice({
-        releaseOrderId: releaseorder._id,
-        insertions: request.body.insertions,
-        releaseOrderNo: releaseorder.releaseOrderNo,
-        MHIDate: request.body.MHIDate,
-        MHIGrossAmount: request.body.MHIGrossAmount,
-        MHITaxAmount: request.body.MHITaxAmount,
-        mediahouseID: mediahouseID,
-        executiveID: executiveID,
-        clientID: clientID,
-        firm: firm._id
-    })
-
-    mhinvoice.save((err,doc)=>{
-        if(err){
-            response.send({
-                success: false,
-                msg: 'media house invoice cannot be created'
+    var token = userController.getToken(request.headers);
+	var user = userController.getUser(token,request,response, async function(err, user){
+		if(err){
+			console.log(err);
+			response.send({
+				success:false,
+				msg:err
+			});
+		}
+		else if(!user){
+			console.log("User not found");
+			response.send({
+				success:false,
+				msg : "User not found, Please Login"
+			});
+		}
+		else{
+            var mediahouseID =await searchMediahouseID(request, response, user);
+            var clientID = await searchClientID(request, response, user);
+            var executiveID = await searchExecutiveID(request, response, user);
+            var releaseorder = await ReleaseOrder.findById(request.body.releaseOrderId);
+            var firm = await Firm.findById(user.firm)
+            
+            var mhinvoice = new MediaHouseInvoice({
+                releaseOrderId: releaseorder._id,
+                insertions: request.body.insertions,
+                releaseOrderNo: releaseorder.releaseOrderNo,
+                MHIDate: request.body.MHIDate,
+                MHIGrossAmount: request.body.MHIGrossAmount,
+                MHITaxAmount: request.body.MHITaxAmount,
+                mediahouseID: mediahouseID,
+                executiveID: executiveID,
+                clientID: clientID,
+                firm: firm._id
             })
-        }
-        else{
+            
+            mhinvoice.save((err,doc)=>{
+                if(err){
+                    response.send({
+                        success: false,
+                        msg: 'media house invoice cannot be created' + err
+                    })
+                }
+                else{
                     response.send({
                         success:true,
                         msg: 'Successfully created the MHInvoice'
                     })
-                
-        }
-    })
+                    
+                }
+            })
+        }	
+	});
 }
 
 
@@ -70,8 +89,8 @@ function searchExecutiveID(request, response, user){
             }
             else if (executive.length===0)
             {
-                    resolve(null);
-            
+                resolve(null);
+                
             }
             if(executive.length!==0){
                 executiveID =  executive[0]._id;
@@ -102,7 +121,7 @@ function searchClientID(request, response, user){
             {
                 
                 resolve(null);
-            
+                
             }
             if(client.length!==0){
                 clientID =  client[0]._id;
@@ -142,29 +161,29 @@ function formQuery(mediahouseID, clientID, executiveID, date, user, request){
         var query = {'firm':user.firm};
         console.log(query)
         console.log(mediahouseID, clientID, executiveID, date, user)
-    if(mediahouseID)
-    query['mediahouseID']=mongoose.mongo.ObjectId(mediahouseID);
-    if(clientID)
-    query['clientID'] = mongoose.mongo.ObjectId(clientID);
-    if(executiveID)
-    query['executiveID']=mongoose.mongo.ObjectId(executiveID);
-    if(request.body.releaseOrderNo){
-        query['releaseOrderNo']=request.body.releaseOrderNo
-    }
-
-    if(request.body.insertionPeriod){
-        to = new Date();
-        from =  new Date(to.getTime() - (request.body.insertionPeriod)*24*60*60*1000);
-        query['insertions.ISODate'] = {$lte:to, $gte:from}
-    }
-    else{
-        to = new Date()
-        from = new Date(1);
-        query['insertions.ISODate'] = {$lte:to, $gte:from}
-    }
-    console.log(to, from);
-    console.log(query)
-    resolve(query);
+        if(mediahouseID)
+        query['mediahouseID']=mongoose.mongo.ObjectId(mediahouseID);
+        if(clientID)
+        query['clientID'] = mongoose.mongo.ObjectId(clientID);
+        if(executiveID)
+        query['executiveID']=mongoose.mongo.ObjectId(executiveID);
+        if(request.body.releaseOrderNo){
+            query['releaseOrderNo']=request.body.releaseOrderNo
+        }
+        
+        if(request.body.insertionPeriod){
+            to = new Date();
+            from =  new Date(to.getTime() - (request.body.insertionPeriod)*24*60*60*1000);
+            query['insertions.ISODate'] = {$lte:to, $gte:from}
+        }
+        else{
+            to = new Date()
+            from = new Date(1);
+            query['insertions.ISODate'] = {$lte:to, $gte:from}
+        }
+        console.log(to, from);
+        console.log(query)
+        resolve(query);
         
     })
     
@@ -190,26 +209,26 @@ module.exports.queryInsertions = function(request, response){
 			});
 		}
 		else{
-                    var mediahouseID =await searchMediahouseID(request, response, user);
-                    var clientID = await searchClientID(request, response, user);
-                    var executiveID = await searchExecutiveID(request, response, user);
-                    var date = (request.body.date)?(request.body.date):null;
-                    var adCategory1 = request.body.adCategory1;
-                    var adCategory2 = request.body.adCategory2;
-                    var to;
-                    var from;
-
-                    var query = await formQuery(mediahouseID, clientID, executiveID, date, user, request);
-
-                    
-                    ReleaseOrder
-                    .aggregate([
-                    {$unwind: "$insertions"}, 
-                    {$match:query},
-                    { $group : { 
-                        _id: "$_id",
-                        count: {$sum: 1},
-                        entries: { $push:  
+            var mediahouseID =await searchMediahouseID(request, response, user);
+            var clientID = await searchClientID(request, response, user);
+            var executiveID = await searchExecutiveID(request, response, user);
+            var date = (request.body.date)?(request.body.date):null;
+            var adCategory1 = request.body.adCategory1;
+            var adCategory2 = request.body.adCategory2;
+            var to;
+            var from;
+            
+            var query = await formQuery(mediahouseID, clientID, executiveID, date, user, request);
+            
+            
+            ReleaseOrder
+            .aggregate([
+                {$unwind: "$insertions"}, 
+                {$match:query},
+                { $group : { 
+                    _id: "$_id",
+                    count: {$sum: 1},
+                    entries: { $push:  
                         {"publicationName":"$publicationName",
                         "publicationEdition":"$publicationEdition", 
                         "clientName":"$clientName",
@@ -225,32 +244,32 @@ module.exports.queryInsertions = function(request, response){
                             "_id": "$insertions._id",
                         }
                     } }
-                     } },
-                    {$limit: perPage},
-                    {$skip:(perPage * request.body.page) - perPage}
-                    ])
-                    .exec(function(err, insertions){
-                                if(err){
-                                    console.log(err+ "");
-                                    response.send({
-                                        success:false,
-                                        msg: err +""
-                                    });
-                                }
-                                else{
-                                    ReleaseOrder.count(query, function(err, count){
-                                        response.send({
-                                            success:true,
-                                            insertions: insertions,
-                                            page: request.body.page,
-                                            perPage:perPage,
-                                            pageCount: Math.ceil(count/perPage)
-                                        });
-                                    })
-                                    
-                                }
-                            });
-                        }	
+                } },
+                {$limit: perPage},
+                {$skip:(perPage * request.body.page) - perPage}
+            ])
+            .exec(function(err, insertions){
+                if(err){
+                    console.log(err+ "");
+                    response.send({
+                        success:false,
+                        msg: err +""
+                    });
+                }
+                else{
+                    ReleaseOrder.count(query, function(err, count){
+                        response.send({
+                            success:true,
+                            insertions: insertions,
+                            page: request.body.page,
+                            perPage:perPage,
+                            pageCount: Math.ceil(count/perPage)
+                        });
+                    })
+                    
+                }
+            });
+        }	
 	});
 };
 
--- api/models/MediaHouseInvoice.js
+++ api/models/MediaHouseInvoice.js
@@ -14,7 +14,7 @@ var MediaHouseInvoiceSchema = new mongoose.Schema({
     },
     insertions:[
         {
-        type:mongoose.Schema.Types.ObjectId, ref:"ReleaseOrder.insertions.$",
+        insertionId:{type:mongoose.Schema.Types.ObjectId, ref:"ReleaseOrder.insertions.$"},
         insertionDate: Date,
         Amount:Number,
         collectedAmount:Number,
@@ -53,8 +53,8 @@ var MediaHouseInvoiceSchema = new mongoose.Schema({
     MediaHouseInvoiceSchema.pre('save', function(next){
         self= this;
         ReleaseOrder.update({releaseOrderId: self.releaseOrderId},{$push: {mediaHouseInvoices: self._id}});
-        self.insertions.array.forEach(element => {
-            element.Amount = (MHIGrossAmount + MHITaxAmount)/self.insertions.array.length;
+        self.insertions.forEach(element => {
+            element.Amount = (self.MHIGrossAmount + self.MHITaxAmount)/self.insertions.length;
         });
         next();
     })
