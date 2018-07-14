ecdde3e00a2842bd313a2ce44265c80547096b68
PranjalSrivastava092 <pranjalsri092@gmail.com>
2018-07-14 16:14:10 +0530

    9 admin routes [ALPHA]

--- admin/routes/router.js
+++ admin/routes/router.js
@@ -2,15 +2,15 @@ var express = require('express');
 var router = express.Router();
 var admin = require('../controllers/adminController');
 
-router.post('/admin/signup', admin.signup);
-router.post('/admin/login', admin.login);
-router.post('/admin/image', admin.profileImage);
-router.delete('/admin/image', admin.deleteProfileImage);
-router.delete('/admin/sign', admin.deleteSignature);
+router.post('/signup', admin.signup);
+router.post('/login', admin.login);
+router.post('/image', admin.profileImage);
+router.delete('/image', admin.deleteProfileImage);
+router.delete('/sign', admin.deleteSignature);
 
-router.post('/admin/sign', admin.signature);
-router.post('/admin/profile', admin.setUserProfile);
-router.post('/admin/changePassword', admin.changePassword);
-router.delete('/admin/:id', admin.deleteUser);
+router.post('/sign', admin.signature);
+router.post('/profile', admin.setUserProfile);
+router.post('/changePassword', admin.changePassword);
+router.delete('/:id', admin.deleteUser);
 
 module.exports = router;
