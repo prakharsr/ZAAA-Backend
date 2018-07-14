58e149e82c2245aea73243f2584074126812e0a5
PranjalSrivastava092 <pranjalsri092@gmail.com>
2018-07-14 16:08:24 +0530

    admin panel middleware

---
+++ admin/middlewares/auth.js
@@ -0,0 +1,62 @@
+var userController = require('../controllers/adminController');
+var Admin = require('../models/Admin');
+var jwt = require('jsonwebtoken');
+var mongoose = require('mongoose');
+var multer = require('multer');
+var mkdirp = require('mkdirp');
+var path = require('path');
+var perPage=20;
+
+
+var unAuthRoutes = [
+    {method:'POST',path:'/api/user/signup'},
+    {method:'POST',path:'/api/user/login'},
+    {method:'POST',path:'/api/user/forgotPassword'}
+];
+
+function getAdmin(token,req,res, cb){
+    var decoded = jwt.verify(token, config.SECRET, function(err,decoded){
+        Admin.findById(decoded.id, function(err, doc) {
+            if (err || !doc) {
+                return  cb(err,null);
+            }
+            else{
+                return cb(null, doc);
+            }
+        });
+    });
+}
+
+function getToken(headers) {
+    if (headers && headers.authorization) {
+        var parted = headers.authorization.split(' ');
+        if (parted.length === 2) {
+            return parted[1];
+        } else {
+            return null;
+        }
+    } else {
+        return null;
+    }
+}
+
+module.exports = function(req, res, next){
+    var query = {method:req.method,path:req.originalUrl};
+    if(JSON.stringify(unAuthRoutes).indexOf(JSON.stringify(query)) >= 0)
+    {
+        next();
+    }
+    else{
+        var token = getToken(req.headers);
+        var admin = getAdmin(token,req,res, function(err, admin){
+            if(err||!admin){
+                console.log(err, admin)
+                console.log("User not found");
+            }
+            else{
+                res.locals.admin = admin;
+                next();
+            }
+        })
+    }
+};
